eab22daf0f3440f3fa0cef251d7c94c1c6334651
Slav Dachev <slav.dachev89@gmail.com>
2018-08-28 10:00:49 +0100

    Try to fix transpile for IE11

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,39 @@
-import * as _ 	from 'lodash'
-import babel 	from 'rollup-plugin-babel'
-import resolve 	from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import pkg 	    from './package.json'
+import * as _ from "lodash";
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import pkg from "./package.json";
 
 const BASE = {
-	external: [
-		'lodash',
-		'vue',
-		'axios'
-	],
-	plugins: [
-		resolve(),
-		commonjs(),
-		babel({
-			babelrc: false,
-			plugins: ["external-helpers"],
-			presets: [["es2015", {modules: false}]],
-			exclude: [
-				'node_modules/**'
-			]
-		}),
-	]
-}
+    external: ["lodash", "vue", "axios"],
+    plugins: [
+        resolve(),
+        commonjs(),
+        babel({
+            babelrc: false,
+            plugins: ["external-helpers"],
+            presets: [["env", { modules: false }], "stage-0"],
+            exclude: ["node_modules/**"],
+        }),
+    ],
+};
 
 const MAIN = _.assign({}, BASE, {
-	input: 'src/index.js',
-	output: [
-		{ file: pkg.main,    format: 'cjs' },
-		{ file: pkg.module,  format: 'es' }
-	],
-})
+    input: "src/index.js",
+    output: [
+        { file: pkg.main, format: "cjs" },
+        { file: pkg.module, format: "es" },
+    ],
+});
 
 const VALIDATION = _.assign({}, BASE, {
-	input: 'src/Validation/index.js',
-	output: [
-		{ file: 'validation/index.js', format: 'cjs' },
-	],
-})
+    input: "src/Validation/index.js",
+    output: [{ file: "validation/index.js", format: "cjs" }],
+});
 
 const LOCALES = _.assign({}, BASE, {
-	input: './src/Validation/locale.js',
-	output: [
-		{ file: 'validation/locale.js', format: 'cjs' },
-	],
-})
+    input: "./src/Validation/locale.js",
+    output: [{ file: "validation/locale.js", format: "cjs" }],
+});
 
-export default [
-	MAIN,
-	VALIDATION,
-	LOCALES,
-]
+export default [MAIN, VALIDATION, LOCALES];
